a8b5db72a626f7547988a7d374f1c98ceb0a20a5
Rafael <rafael.felix.moraes@gmail.com>
2025-06-20 23:03:11 -0300

    Refactor add função com parametros

--- assets/js/variaveis.js
+++ assets/js/variaveis.js
@@ -6,3 +6,10 @@ let nomeCompletoObjeto = {
     nome: "Pedro",
     idade: 10
 };
+
+function somar(n1, n2) {
+    let resultado = n1 + n2;
+    console.log('Resultado: ' + resultado);
+}
+
+somar(10, 15);
