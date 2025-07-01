fbbb3fe48baa14d8b4f712e0da8a45ce09560314
Rafael <rafael.felix.moraes@gmail.com>
2025-07-01 16:22:04 -0300

    Refactor add getElements

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,1 +1,5 @@
-//document.getElementsByTagName("h1") retorna 1 array com todos os elementos com h1 achados no documento.
+// sempre que getElements for no plural, retorna um array
+
+//document.getElementsByTagName("h1") retorna 1 array com todos os elementos com h1 achados no documento. (não é a forma mais eficiente)
+
+//document.getElementById("teste") caso especifico, quando houver 1 elemento apenas.
