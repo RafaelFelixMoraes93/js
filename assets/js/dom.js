688bf8c182f8989f0acb9a58eeb3c016fc89b01d
Rafael <rafael.felix.moraes@gmail.com>
2025-07-01 16:25:09 -0300

    Refactor mais forma de select dom

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -3,3 +3,5 @@
 //document.getElementsByTagName("h1") retorna 1 array com todos os elementos com h1 achados no documento. (não é a forma mais eficiente)
 
 //document.getElementById("teste") caso especifico, quando houver 1 elemento apenas.
+
+//document.querySelector(".botao") seleciona igual no css e inclui os niveis de camadas #teste ul li
